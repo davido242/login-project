71e8981291790d213f9d3fb1b0605230a4da40a7
David042 <Davidsarka242@gmail.com>
2021-03-20 09:57:53 -0700

    Created the login Page

--- app/Views/form.php
+++ app/Views/form.php
@@ -17,6 +17,19 @@
 		<input type="submit" value="Login Now" name="submit">
 	</form>
 </div>
+<div class="form_container_left">
+	<h1>WHO WE ARE</h1>
+	<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.	
+	</p>
+	<button>Learn More</button>
+	
+</div>
 
 
 <!-- <div class="form_container">
--- app/Views/index.php
+++ app/Views/index.php
@@ -9,31 +9,9 @@
 <?php echo view("form");?>
 
 
-
-<?php echo view("piller/footer");?>
 <!-- background image address
 https://cdn.mos.cms.futurecdn.net/TeHDvyZxpKKpfZDQoKUySj-970-80.jpg.webp -->
+
+<?php echo view("piller/footer");?>
 </body>
 </html>
-
-
-
-<!-- <div class="form_container">
-  <h1>Login</h1>
-  <form class="form_control" action="success" method="post">
-    <div class="label">*Log into your account</div>
-    <i class="fa fa-user 3x"></i>
-    <input type="email" name="name" placeholder="Your Email">
-    <div class="error_message">
-      Invalid Email or Phone input!
-    </div>
-    <div class="label">*Password</div>
-    <i class="fa fa-user fa-lock 3x"></i>
-    <input type="password" name="password" placeholder="Password">
-    <div class="error_password">
-      Invalid Password Input!
-    </div>
-    <div class="label">Forgot Password?</div>
-    <input type="submit" value="Login Now" name="submit">
-  </form>
-</div> -->
