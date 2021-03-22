3659ce1a36cf84b4126fc32aeb936edd5cd464ae
David042 <Davidsarka242@gmail.com>
2021-03-22 11:21:04 -0700

    Logo Cleanup

--- app/Views/piller/header.php
+++ app/Views/piller/header.php
@@ -4,4 +4,3 @@
 <!-- font Awesome CDN -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/nav-style.css');?>">
--- app/Views/piller/nav_bar.php
+++ app/Views/piller/nav_bar.php
@@ -6,7 +6,7 @@
       <i class="fa fa-bars"></i>
     </label>
     <div class="logo">
-      <img src="https://raw.githubusercontent.com/davido242/login-project/main/public/images/testrunlogo.png" alt="Clients/Company Logo">
+      <img src="https://raw.githubusercontent.com/davido242/login-project/main/public/images/testrun2.png" alt="Clients/Company Logo">
     </div>
     <div class="menu-holder">
       <ul>
