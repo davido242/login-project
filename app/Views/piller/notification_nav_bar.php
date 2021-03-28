279d72b06e328516dc077386853cb7f4e0e38f52
David042 <Davidsarka242@gmail.com>
2021-03-28 09:38:16 -0700

    Toglle nav bar-main on notification page

--- app/Views/piller/notification_nav_bar.php
+++ app/Views/piller/notification_nav_bar.php
@@ -1,24 +1,21 @@
 <header class="chat-header">
+  <div class="logo2">
+    <img src="https://raw.githubusercontent.com/davido242/login-project/main/public/images/testrun2.png" alt="Clients/Company Logo">
+  </div>
   <i id="collapseNavBar" class="fa fa-bars chat-bars" onclick="myFunction()"></i>
-    <div class="logo2">
-      <img src="https://raw.githubusercontent.com/davido242/login-project/main/public/images/testrun2.png" alt="Clients/Company Logo">
-    </div>
 </header>
 
-<!-- Nav-bar Section -->
-
-<nav class="chat-nav">
-  <div class="menu-container menu-container-chat">
+<main>
+  <!-- Nav-bar Section -->
+  <nav class="chat-nav mystyle" id="myDIV">
     <div class="notification-box">
-      <div class="image-container-h">
-        <div class="image-circle">
-          <!-- empty circle -->
-        </div>
+      <div class="image-circle">
+        <!-- empty circle -->
       </div>
-        <div class="caption-holder">
-          <p>John Agbaeze</p>
-          <p>Enugu, NG</p>
-        </div>
+      <div class="caption-holder">
+        <p>John Agbaeze</p>
+        <p>Enugu, NG</p>
+      </div>
     </div>
     <ul>
       <li><span><i class="fa fa-bell note-bars"></i></span><a href="#">Notification</a></li>
@@ -27,11 +24,10 @@
       <li><span><i class="fa fa-share-square note-bars"></i></span><a href="#">Subscription</a></li>
       <li><span><i class="fa fa-power-off note-bars"></i></span><a href="#">Logout</a></li>
     </ul>
-  </div>
-</nav>
+  </nav>
 
-<!-- notification section -->
-<div class="notification-stat-main">
+  <!-- notification section -->
+  <div class="notification-stat-main">
     <div class="notification-stat">
       <i class="fa fa-bell note-bars"></i><a href="#">Notifications</a>
     </div>
@@ -54,6 +50,6 @@
           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
           </span>
       </p>
-    </div>
-      
-</div>
+    </div>  
+  </div>
+</main>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,8 +14,8 @@ function showRegistrationForm(){
 //   document.getElementById('myDemo').style.display = 'none';
 //   document.getElementById('myDemo2').style.display = 'block';
 // }
-
-function collapseNavBarSec(){
-  document.getElementById('myDemo').style.display = 'none';
-  document.getElementById('myDemo2').style.display = 'block';
+function myFunction() {
+  console.trace();
+   var element = document.getElementById("myDIV");
+  element.classList.toggle("mystyle");
 }
